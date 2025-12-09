90d1556c8bee365bbf24558a7917ee39cf29c971
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-12-10 01:40:28 +0200

    Custom 404 Not Found Add

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h1 className='text-6xl text-red-600 text-center'>!!!404 NOT FOUND!!!</h1>
+        </div>
+    );
+};
+
+export default NotFoundPage;
