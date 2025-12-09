c09b53dc865103e7149a0b1d746280decd457f6d
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-12-10 01:47:01 +0200

    Custom Loading Added

---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+const LoadingPage = () => {
+    return (
+        <div>
+            <h1 className="text-6xl text-red-500 text-center my-10">Loading....</h1>
+        </div>
+    );
+};
+
+export default LoadingPage;
