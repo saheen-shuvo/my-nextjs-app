9dba77688896e6475e197e865ace9b3bdf010fb5
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-12-09 22:10:22 +0200

    searchParams and Catch All Routes

--- src/app/products/[productId]/page.js
+++ src/app/products/[productId]/page.js
@@ -1,6 +1,8 @@
-const DynamicPage = async ({ params }) => {
+const DynamicPage = async ({ params, searchParams }) => {
   const {productId} = await params;
+  const {category} = await searchParams;
   console.log(productId);
+  console.log(category);
   return (
     <div>
       <h1>Dynamic Product Page {productId}</h1>
