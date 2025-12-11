859cf3882a0dd6b363c8c6c912ccbf037ef15070
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-12-11 21:27:32 +0200

    New Google Font Added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Jost } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Shared/Navbar/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const jost = Jost({
+  subsets: ['latin'],
+})
 
 export const metadata = {
   title: "My Next.js App",
@@ -21,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={jost.className}
       >
         <Navbar></Navbar>
         {children}
